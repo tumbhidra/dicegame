1cf909b0d2a2193eceeb94d5c0ab4714ae1d3c63
Rohith Naidu <112018257+tumbhidra@users.noreply.github.com>
2026-07-18 15:36:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 console.log("JavaScript is running");
 var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 var randomNumber2 = Math.floor(Math.random() * 6) + 1;
-var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";
-var randomDiceImage2 = "images/dice" + randomNumber2 + ".png";
+var randomDiceImage1 = "dice" + randomNumber1 + ".png";
+var randomDiceImage2 = "dice" + randomNumber2 + ".png";
 var header = document.querySelector("h1");
 document.querySelector(".img1").src = randomDiceImage1;
 document.querySelector(".img2").src = randomDiceImage2;
